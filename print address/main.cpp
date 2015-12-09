7058a2d428e518a1be2cf0ee2d635bcc7a99f975
20141105073ld <a20141105073@bieliangewifi.local>
2015-12-09 21:09:20 +0800

    Initial Commit

---
+++ print address/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  print address
+//
+//  Created by 20141105073 on 15/12/9.
+//  Copyright © 2015年 20141105073. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
