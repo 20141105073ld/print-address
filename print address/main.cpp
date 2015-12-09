c55f94a2ef0d3991e97631fa3860c5fa36685d77
20141105073ld <a20141105073@bieliangewifi.local>
2015-12-09 21:11:58 +0800

    print address
    地址打印，将变量a的地址赋值给指针变量p，最后以十六进制输出地址。

--- print address/main.cpp
+++ print address/main.cpp
@@ -5,11 +5,15 @@
 //  Created by 20141105073 on 15/12/9.
 //  Copyright © 2015年 20141105073. All rights reserved.
 //
-
+#include <iostream>
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    
+    printf("a=%x &a=%x &p=%x p=%x *p=%x\n",a,&a,&p,p,*p);
     return 0;
 }
